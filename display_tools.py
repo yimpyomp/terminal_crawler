dbd23f20f2a7dd589421faf299c2b3e4ef9a7f76
yimpyomp <sebastian.enzi@gmail.com>
2026-06-22 20:22:29 -0500

    implement helper

--- display_tools.py
+++ display_tools.py
@@ -6,7 +6,7 @@ CONTENT_START_COL = 1
 INFO_START_ROW = 1
 
 
-def draw_map(screen, level, player, header_divider):
+def draw_map(screen, level, player, map_start_row):
     for row_number, row in enumerate(level):
         display_line = ''
         for col_number, tile in enumerate(row):
@@ -14,19 +14,21 @@ def draw_map(screen, level, player, header_divider):
                 display_line += '@'
             else:
                 display_line += tile
-        screen.addstr(header_divider + row_number + 1, 1, display_line)
+        screen.addstr(map_start_row + row_number + 1, 1, display_line)
 
 
 def calculate_layout_positions(screen):
     height, width = screen.getmaxyx()
+    usable_width = width - 1
     header_divider = HEADER_HEIGHT - 1
     message_divider = height - MESSAGE_HEIGHT
     sidebar_divider = width - SIDEBAR_WIDTH
     map_start_row = header_divider + 1
     message_row = message_divider + 1
-    sidebar_start_col = sidebar_divider + 1
+    sidebar_start_col = sidebar_divider + 2
     layout = {"height": height,
               "width": width,
+              "usable_width": usable_width,
               "header_divider": header_divider,
               "message_divider": message_divider,
               "sidebar_divider": sidebar_divider,
@@ -61,39 +63,35 @@ def draw_layout(screen):
     height, width = screen.getmaxyx()
     width -= 1
 
+    layout_positions = calculate_layout_positions(screen)
+
     # Draw box overlay
     box_screen(screen)
 
-    # Define where additional borders will be
-    header_divider = HEADER_HEIGHT - 1
-    message_divider = height - MESSAGE_HEIGHT
-    sidebar_divider = width - SIDEBAR_WIDTH
-
     # Draw horizontal dividers
-    screen.addstr(header_divider, 0, build_horizontal_line(width))
-    screen.addstr(message_divider, 0, build_horizontal_line(width))
+    screen.addstr(layout_positions["header_divider"], 0, build_horizontal_line(width))
+    screen.addstr(layout_positions["message_divider"], 0, build_horizontal_line(width))
 
     # Draw vertical divider
-    for row in range(HEADER_HEIGHT, message_divider):
-        screen.addstr(row, sidebar_divider, '|')
+    for row in range(HEADER_HEIGHT, layout_positions["message_divider"]):
+        screen.addstr(row, layout_positions["sidebar_divider"], '|')
 
     # Add the corner pieces for the sidebar
-    screen.addstr(header_divider, sidebar_divider, '+')
-    screen.addstr(message_divider, sidebar_divider, '+')
+    screen.addstr(layout_positions["header_divider"], layout_positions["sidebar_divider"], '+')
+    screen.addstr(layout_positions["message_divider"], layout_positions["sidebar_divider"], '+')
 
 
 def box_screen(screen):
-    height, width = screen.getmaxyx()
-    width -= 1
+    layout_positions = calculate_layout_positions(screen)
 
-    horizontal_border = '+' + '-' * (width - 2) + '+'
+    horizontal_border = '+' + '-' * (layout_positions["usable_width"] - 2) + '+'
     screen.addstr(0, 0, horizontal_border)
 
-    side_border = '|' + ' ' * (width - 2) + '|'
-    for row in range(1, height - 1):
+    side_border = '|' + ' ' * (layout_positions["usable_width"] - 2) + '|'
+    for row in range(1, layout_positions["height"] - 1):
         screen.addstr(row, 0, side_border)
 
-    screen.addstr(height - 1, 0, horizontal_border)
+    screen.addstr(layout_positions["height"] - 1, 0, horizontal_border)
 
 
 
